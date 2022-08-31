0c364a2c8ff734230cbfce26a9f99d1cc75c4f4f
Ansis Maleckis <maleckisansis@gmail.com>
2022-08-31 18:53:21 +0300

    some progress

--- src/main/java/com/chemapps/ChemScraper/controllers/HtmlController.java
+++ src/main/java/com/chemapps/ChemScraper/controllers/HtmlController.java
@@ -1,7 +1,12 @@
 package com.chemapps.ChemScraper.controllers;
 
+import com.chemapps.ChemScraper.CodeInput;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HtmlController {
@@ -12,4 +17,18 @@ public class HtmlController {
         System.out.println("HeiHei");
         return "test";
     }
+
+    @GetMapping("/search")
+    public String searchForm() {
+        System.out.println("YoYo");
+        return "search";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        System.out.println("form");
+        return "form";
+    }
+
+
 }
