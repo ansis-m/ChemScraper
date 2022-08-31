9ae0d34b8e66d3239c78adc5ced2e5294cfbcdf0
Ansis Maleckis <maleckisansis@gmail.com>
2022-08-31 10:19:17 +0300

    initialized the project

---
+++ src/main/java/com/chemapps/ChemScrapper/ChemScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.chemapps.ChemScrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chemapps/ChemScrapper/ChemScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chemapps.ChemScrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
